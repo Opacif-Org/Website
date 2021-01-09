13656ff79aeaae101ad9cccd7e253ec86e5c9252
Norlan Madrigal Hidalgo <madrigalnorlan8@gmail.com>
2021-01-09 14:30:45 -0600

    Agrego de Logos
    Se agrego el logo de Kennis a programas y tambien los logos de los objetivos de desarrollo sostenible

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -25,7 +25,7 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
+$EmailTo = "madrigalnorlan8@gmail.com";
 $Subject = "New message from Evolo landing page";
 
 // prepare email body text
